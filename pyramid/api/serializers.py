c936e35dc89e3bfd7807b5937478e5e0a4680398
claudiu.murgea <claudiu.murgea@cms.lan>
2025-04-23 09:16:51 +0300

    initial

---
+++ pyramid/api/serializers.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import Note
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        # checks on both ways, coming and going
+        fields = ["id", "username", "password"]
+        # we want to accept passwrd when we are accepting but we don't want to return a password
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ["id", "title", "content", "created_at", "author"]
+        extra_kwargs = {"author": {"read_only": True}}
---
+++ pyramid/pyramid/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshSlidingView
+from api.views import CreateUserView
+from auth.login import CustomTokenObtainPairView
+from auth.logout import LogoutView
+# prebuilt views, that allow us to obtain and access our refresh token, bult in to sign in users
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/user/register/', CreateUserView.as_view(), name="register"),
+    path('api/token/', CustomTokenObtainPairView.as_view(), name="get_token"),
+    path('api/token/refresh', TokenRefreshSlidingView.as_view(), name="refresh"),
+    path('api-auth/', include("rest_framework.urls")),
+    path('api/logout/', LogoutView.as_view(), name="logout"),
+    path('api/', include('api.urls')),
+    path("test/", include("myapp.urls"))
+]
